8156ae3048223941f552b119732550c5b4e65264
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-16 13:14:25 -0700

    add passing earthDays test

--- src/galactic.js
+++ src/galactic.js
@@ -2,6 +2,7 @@ export class Solar {
   constructor(age, lifeExpectancy) {
     this.age = age;
     this.lifeExpectancy = lifeExpectancy;
+    this.earthDays = 365;
     this.yearsLeft = this.lifeExpectancy - this.age;
   }
 
